294e19070646eda0737ff4fd8878915e0b0b65fe
sreysus <sreyassabbani@gmail.com>
2025-07-22 20:02:01 -0400

    remove old files

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  printWidth: 100,
-  tabWidth: 4,
-  singleQuote: true,
-  bracketSameLine: true,
-  trailingComma: 'es5',
-  
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  tailwindAttributes: ["className"],
-  
+    printWidth: 100,
+    tabWidth: 4,
+    bracketSameLine: true,
+    trailingComma: "es5",
+
+    plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    tailwindAttributes: ["className"],
 };
